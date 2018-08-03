f219efba9bc5ae052ee14817d46c7b24af4393c8
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-08-03 14:45:52 +0900

    add completion function

---
+++ _second.zsh
@@ -0,0 +1,51 @@
+function _second() {
+  local ret=1
+
+  function sub_commands() {
+    local -a _c
+
+    _c=(
+      'register' \
+      'change' \
+      'list' \
+      'delete'
+    )
+
+    _describe -t commands Commands _c
+  }
+
+  _arguments -C \
+    '(-h --help)'{-h,--help}'[show help]' \
+    '1: :sub_commands' \
+    '*:: :->args' \
+    && ret=0
+
+  case ${state} in
+    (args)
+      case ${words[1]} in
+        (register)
+          _arguments \
+            '(-n --name)'{-n,--name}'[Second name]' \
+            '(-p --path)'{-p,--path}'[Target path]'
+        ;;
+        (change)
+          _values \
+            'Second names' \
+            $(second list --name)
+        ;;
+        (list)
+          _arguments \
+            '(-n --name)'{-n,--name}'[Second name]' \
+            '(-p --path)'{-p,--path}'[Target path]'
+        ;;
+        (delete)
+          _values \
+            'Second names' \
+            $(second list --name)
+        ;;
+      esac
+  esac
+
+  return ret
+}
+compdef _second second
