4b055806650ab251beb5e216fdd3b5a8d31ee7f9
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-09-09 13:19:26 +0900

    [fix] Do not display eval's error message

--- second.zsh
+++ second.zsh
@@ -1,6 +1,6 @@
 function second() {
   local second=${GOPATH}/bin/second
   [[ $1 == 'change' ]] \
-    && eval cd $(${second} $@ || echo '.') \
+    && eval cd "$(${second} $@ || echo '.')" 2> /dev/null \
     || ${second} $@
 }
