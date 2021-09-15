9be88a94f5236035fb247a47e6a950f5181b8900
atsuya0 <37957375+atsuya0@users.noreply.github.com>
2021-09-15 23:57:36 +0900

    Move the data-related code to the separate file

--- cmd/utils.go
+++ cmd/utils.go
@@ -3,60 +3,9 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 )
-
-func getEnvPath() (string, error) {
-	path := os.Getenv("SCD_LIST_PATH")
-	if path == "" {
-		return "", errors.New("Cannot get path what use defined env")
-	}
-	return path, nil
-}
-
-func getXdgPath() (string, error) {
-	conf := os.Getenv("XDG_CONFIG_HOME")
-	if conf == "" {
-		return "", errors.New("Cannot get path what use XDG_CONFIG_HOME")
-	}
-	path := filepath.Join(conf, "scd")
-	if err := os.MkdirAll(path, 0700); err != nil {
-		return "", err
-	}
-
-	return filepath.Join(path, "list.json"), nil
-}
-
-func getConfPath() (string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	path := filepath.Join(homeDir, ".config", "scd")
-	if err := os.MkdirAll(path, 0700); err != nil {
-		return "", err
-	}
-
-	return filepath.Join(path, "list.json"), nil
-}
-
-func getSecondPath() (string, error) {
-	if path, err := getEnvPath(); err == nil {
-		return path, nil
-	}
-	if path, err := getXdgPath(); err == nil {
-		return path, nil
-	}
-	if path, err := getConfPath(); err != nil {
-		return path, nil
-	} else {
-		return "", fmt.Errorf("Cannot get the file path to save the data: %w", err)
-	}
-}
 
 func getSecond() (second, error) {
 	path, err := getSecondPath()
